18dcfed13d24669afcea71dae160ca88bc3b666c
Isshon-Saha <isaha367@gmail.com>
2024-01-30 00:38:23 +0600

    fixed a bug with productlist not showing properly

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -10,19 +10,20 @@ export const revalidate = 0;
 
 const MainPage = async () => {
 	const billboardPromise = getBillboard("3f07e84a-45fb-44e1-ac75-b7c055eb1c3e");
-	const productsPromise = getProducts({ isFeatured: true });
+	const featuredProductsPromise = getProducts({ isFeatured: true });
+	const notFeaturedProducts = await getProducts({ isFeatured: false });
 
 	noStore();
 
-	const res = await Promise.all([billboardPromise, productsPromise]);
-	const [billboard, products] = res;
+	const res = await Promise.all([billboardPromise, featuredProductsPromise]);
+	const [billboard, featuredProducts] = res;
 	return (
 		<Container>
 			<div className="space-y-10 pb-10">
 				<Billboard data={billboard} />
 			</div>
 			<div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-				<ProductList title="Featured Products" products={products} />
+				<ProductList title="Featured Products" products={featuredProducts} />
 			</div>
 		</Container>
 	);
